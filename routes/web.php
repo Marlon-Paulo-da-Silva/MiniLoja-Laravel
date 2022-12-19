e04d6f6eadd9eade27c1710d343d534fc255d812
Marlon Paulo <marlon.paulo.silva@outlook.com>
2022-12-19 18:10:35 -0300

    Att first commit testando o framework

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// rota de query opcional, colocar o ponto de interrogação
+Route::get('/empresa/{string?}', function ($string = null) {
+    echo "O nome da empresa é: <br><br>";
+    return $string;
+});
+
 Route::get('/', function () {
+    
     return view('welcome');
+});
+
+Route::prefiz('/', function () {
+    
+    return view('welcome');
+});
+
+// TODO INTRO #4 13:53
+
+Route::get('/users/{user}', function (\App\Models\User $user) {
+    
+    return $user;
 });
