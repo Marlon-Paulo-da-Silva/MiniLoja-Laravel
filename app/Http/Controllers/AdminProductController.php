51396055e63d8418d9c8b45cc2e0a9349b0fe172
Marlon Paulo <marlon.paulo.silva@outlook.com>
2022-12-27 17:38:13 -0300

    Cadastrando imagem

--- app/Http/Controllers/AdminProductController.php
+++ app/Http/Controllers/AdminProductController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Str;
 
 class AdminProductController extends Controller
 {
@@ -30,8 +32,29 @@ class AdminProductController extends Controller
     }
 
     // recebe a requisição para criar POST
-    public function store(){
-        return view('admin.product_create');
+    public function store(Request $request){
+        $input = $request->validate([
+            'title' => 'string|required',
+            'price' => 'string|required',
+            'stock' => 'integer|nullable',
+            'cover' => 'file|nullable',
+            'description' => 'string|nullable'
+        ]);
+
+        $input['slug'] = Str::slug($input['title']);
+
+        if(!empty($input['cover']) && $input['cover']->isValid()){
+
+            $file = $input['cover'];
+            $path = $file->store('products');
+            $input['cover'] = $path;
+
+            // dd($input['cover']);
+        }
+
+        Product::create($input);
+
+        return Redirect::route('admin.products');
     }
     
 }
